bbd1105825d7b427667bab7e1379609df9e4426b
Edwin Cardenas <ecardenas@lwolf.com>
2025-01-14 15:29:18 -0500

    feat: use global variables

--- Constants/main.m
+++ Constants/main.m
@@ -11,6 +11,13 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         NSLog(@"\u03c0 is %f", M_PI);
         NSLog(@"%d is larger", MAX(12, 10));
+        
+        NSLocale *here = [NSLocale currentLocale];
+        NSString *currency = [here objectForKey:@"currency"];
+        
+        NSLog(@"Money is %@", currency);
+        NSLog(@"Money is %@", [here currencyCode]);
+        NSLog(@"Money is %@", [here objectForKey:NSLocaleCurrencyCode]); // Foundation global variable
     }
     return 0;
 }
